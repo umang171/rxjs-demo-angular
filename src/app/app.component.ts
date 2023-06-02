01111f6cb6cbfbf36a541be8b3b4c597e57ab4e2
Umang Gohel <gohelumang12@gmail.com>
2023-06-02 17:49:41 +0530

    First object of Observable with subscriber and observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { compileClassMetadata } from '@angular/compiler';
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'rxjs-demo';
+  // First Observable
+  observable=new Observable(subscriber=>{
+    subscriber.next("Hello");
+    subscriber.next("Umang");
+    subscriber.complete();
+  });
+
+  observer={
+    next:(value: any)=>console.log(value),
+    error:(error:any)=>console.error(error),
+    complete:()=>console.log("completed")
+  }
+
+  constructor(){
+    this.observable.subscribe(this.observer);
+  }
 }
