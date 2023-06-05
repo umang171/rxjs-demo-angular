0aad74bca2b9abfd9b266120a951c01beb6e1ec3
Umang Gohel <gohelumang12@gmail.com>
2023-06-05 09:35:08 +0530

    fromEvent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { compileClassMetadata } from '@angular/compiler';
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, fromEvent } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,33 +8,16 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   // First Observable
-  observable=new Observable(subscriber=>{
-    let count=0;
-    const id=setInterval(()=>{
-      subscriber.next(count);
-      // subscriber.complete();
-      count++;
-    },1000);
-    return ()=>{
-      console.log("Called");
-      clearInterval(id);
-    }
-  });
+  observable=fromEvent(document,"click");
 
   observer={
-    next:(value: any)=>console.log(value),
+    next:(value: any)=>console.log("next-",value),
     error:(error:any)=>console.error(error),
     complete:()=>console.log("completed")
   }
 
   constructor(){
-    console.log("before");
     const sub1=this.observable.subscribe(this.observer);
     const sub2=this.observable.subscribe(this.observer);
-    console.log("after");    
-    setTimeout(()=>{
-      sub1.unsubscribe();
-      sub2.unsubscribe();
-    },3500);
   }
 }
