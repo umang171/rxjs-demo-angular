335e6fc1944f025bd74beba57833738684b93997
Umang Gohel <gohelumang12@gmail.com>
2023-06-05 11:34:51 +0530

    pluck and map with keyboard event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { compileClassMetadata } from '@angular/compiler';
 import { Component } from '@angular/core';
-import { filter, from, map, mapTo } from 'rxjs';
+import { filter, from, fromEvent, map, mapTo, pluck } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  observable=from([1,2,3,4,5]);
+  observable=fromEvent(document,"keyup");
   constructor(){
     this.observable.pipe(
-      mapTo("Hello umang"),
+      // map(event=>(event as KeyboardEvent).code)
+      pluck('code')
     )
     .subscribe(console.log);
   }
