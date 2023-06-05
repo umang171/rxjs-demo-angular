4749398dac16ecddf5289304ef886759a564ea03
Umang Gohel <gohelumang12@gmail.com>
2023-06-05 11:25:16 +0530

    interval and timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,16 @@
 import { compileClassMetadata } from '@angular/compiler';
 import { Component } from '@angular/core';
-import { Observable, from, fromEvent, of, range } from 'rxjs';
+import { Observable, from, fromEvent, interval, of, range, timer } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  observable=of(1,2,3,4,5);
-  observable2=from([1,2,3,4,5]);
-  observable3=from(fetch('https://api.github.com/users/octocat'));
-  iterator=this.hello();
-  observable4=from(this.iterator);
-  observer={
-    next:(value: any)=>console.log("next-",value),
-    error:(error:any)=>console.error(error),
-    complete:()=>console.log("completed")
-  }
-  
+  // observable=interval(1000);
+  observable=timer(0,1000);
   constructor(){
-    const sub=this.observable.subscribe(this.observer);
-    const sub2=this.observable2.subscribe(this.observer);
-    const sub1=this.observable3.subscribe(this.observer);
-    const sub4=this.observable4.subscribe(this.observer);
+    this.observable.subscribe(console.log);
   }
   
-  *hello() {
-    yield 'Hello';
-    yield 'World';
-  }
 }
