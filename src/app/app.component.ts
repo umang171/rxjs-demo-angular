b307919751dc70ebfbffde45ba0bd984af3df731
Umang Gohel <gohelumang12@gmail.com>
2023-06-05 09:55:09 +0530

    from operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,32 @@
 import { compileClassMetadata } from '@angular/compiler';
 import { Component } from '@angular/core';
-import { Observable, fromEvent, of, range } from 'rxjs';
+import { Observable, from, fromEvent, of, range } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // First Observable
-  observable=of([1],2,3,4,5);
-  observable2=range(1,5);
-
+  observable=of(1,2,3,4,5);
+  observable2=from([1,2,3,4,5]);
+  observable3=from(fetch('https://api.github.com/users/octocat'));
+  iterator=this.hello();
+  observable4=from(this.iterator);
   observer={
     next:(value: any)=>console.log("next-",value),
     error:(error:any)=>console.error(error),
     complete:()=>console.log("completed")
   }
-
+  
   constructor(){
-    const sub1=this.observable.subscribe(this.observer);
+    const sub=this.observable.subscribe(this.observer);
     const sub2=this.observable2.subscribe(this.observer);
+    const sub1=this.observable3.subscribe(this.observer);
+    const sub4=this.observable4.subscribe(this.observer);
+  }
+  
+  *hello() {
+    yield 'Hello';
+    yield 'World';
   }
 }
